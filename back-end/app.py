cd73c8a898607b4dbdc3e66de32871f02a6add6e
Jenny Lei <jenny.h.lei@hotmail.com>
2022-02-12 15:49:52 -0500

    unhardcode database uri

--- back-end/app.py
+++ back-end/app.py
@@ -4,7 +4,11 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Jenny:@localhost:5432/baddie'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{user}:{password}@{host}/{name}'.format(
+    user=os.getenv('DATABASE_USER'),
+    password=os.getenv('DATABASE_PASSWORD'),
+    host=os.getenv('DATABASE_HOST'),
+    name=os.getenv('DATABASE_NAME'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
