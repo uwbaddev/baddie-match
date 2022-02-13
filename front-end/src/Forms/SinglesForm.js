024e3585735770b7d0ae48448b9384f66df5718d
Angela Chen <angelachen085@gmail.com>
2022-02-13 10:29:51 -0500

    Mobile support

--- front-end/src/Forms/SinglesForm.js
+++ front-end/src/Forms/SinglesForm.js
@@ -70,30 +70,30 @@ const SinglesForm = () => {
                 <Col className='page-header'>Score</Col>
             </Row>
             <Row>
-                <Col xs={6} md={9} ></Col>
-                <Col xs={2} md={1} >GAME 1</Col>
-                <Col xs={2} md={1} >GAME 2</Col>
-                <Col xs={2} md={1} >GAME 3</Col>
+                <Col xs={3} md={9} ></Col>
+                <Col xs={3} md={1} >GAME 1</Col>
+                <Col xs={3} md={1} >GAME 2</Col>
+                <Col xs={3} md={1} >GAME 3</Col>
             </Row>
             <Row>
-                <Col xs={6} md={9} >PLAYER ONE</Col>
-                <Col xs={2} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
-                <Col xs={2} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
-                <Col xs={2} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
+                <Col xs={3} md={9} >PLAYER ONE</Col>
+                <Col xs={3} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
+                <Col xs={3} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
+                <Col xs={3} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
             </Row>
             <hr></hr>
             <Row>
-                <Col xs={6} md={9} >PLAYER TWO</Col>
-                <Col xs={2} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
-                <Col xs={2} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
-                <Col xs={2} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
+                <Col xs={3} md={9} >PLAYER TWO</Col>
+                <Col xs={3} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
+                <Col xs={3} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
+                <Col xs={3} md={1} ><Form.Control type='number' min='0' max='30'></Form.Control></Col>
             </Row>
 
             <Row>
                 <Col className='page-header'>CATEGORY</Col>
             </Row>
             <Row>
-                <Col xs={6} md={4}>
+                <Col xs={10} md={4}>
                     <Form.Select>
                         <option>Choose an option</option>
                         {categories.map((c, i) => <option key={i} value={c.name}>{c.name}</option>)}
