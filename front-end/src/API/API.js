f4bce59abaf32c6f5b6fa4973961dc12f238f372
Angela Chen <angelachen085@gmail.com>
2022-02-13 10:07:13 -0500

    Fix error in player url fetch

--- front-end/src/API/API.js
+++ front-end/src/API/API.js
@@ -2,4 +2,4 @@ export const DomainName = "https://baddie-match.herokuapp.com";
 export const ResultsURL = `${DomainName}/results`;
 export const ReportUrl = `${DomainName}/report`;
 export const CategoryUrl = `${DomainName}/categories`;
-export const PlayerUrl = `${DomainName}/player`;
+export const PlayerUrl = `${DomainName}/players`;
