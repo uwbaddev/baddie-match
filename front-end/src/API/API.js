035ef1f0362f53313451239a021ce9fcdfd7fae1
Darren Choi <75344587+choiii-darren@users.noreply.github.com>
2022-02-12 23:28:33 -0500

    navbar change

--- front-end/src/API/API.js
+++ front-end/src/API/API.js
@@ -1,5 +1,5 @@
 export const DomainName = "https://baddie-match.herokuapp.com";
-export const ResultsURL = `${DomainName}/results`
-export const ReportUrl = `${DomainName}/report`
+export const ResultsURL = `${DomainName}/results`;
+export const ReportUrl = `${DomainName}/report`;
 export const CategoryUrl = `${DomainName}/categories`;
-export const PlayerUrl = `${DomainName}/player`
+export const PlayerUrl = `${DomainName}/player`;
