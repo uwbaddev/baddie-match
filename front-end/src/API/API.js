2cb38a27aacbcbb8a0724496492c894f30d61a98
Angela Chen <angelachen085@gmail.com>
2022-02-13 21:17:11 -0500

    Correct routes and add alpha ordering

--- front-end/src/API/API.js
+++ front-end/src/API/API.js
@@ -1,5 +1,5 @@
-export const DomainName = "https://baddie-match.herokuapp.com";
+export const DomainName = "https://baddie-match.herokuapp.com/api";
 export const ResultsURL = `${DomainName}/results`;
-export const ReportUrl = `${DomainName}/report`;
+export const ReportUrl = `${DomainName}/match`;
 export const CategoryUrl = `${DomainName}/categories`;
 export const PlayerUrl = `${DomainName}/players`;
--- front-end/src/Contexts/AppContext.js
+++ front-end/src/Contexts/AppContext.js
@@ -13,14 +13,13 @@ const useApp = () => {
         fetch(PlayerUrl, { method: 'GET' })
             .then(res => res.json())
             .then(data => {
+                data.sort((a,b) => (a.first_name > b.first_name) ? 1 : ((b.first_name > a.first_name) ? -1 : 0))
                 setPlayers(data);
             });
         fetch(CategoryUrl, { method: 'GET' })
             .then(response => response.json())
             .then(data => { setCategories(data) })
     }, []);
-
-
 
     return {
         categories,
