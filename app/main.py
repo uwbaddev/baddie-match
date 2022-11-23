a720770a7d29edc3903952c0a290a71b81768c48
ivanccheng <chengivanc@gmail.com>
2022-11-23 16:45:25 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ from flask_cors import CORS
 import os
 from dotenv import load_dotenv
 
+
+print(os.path.abspath(os.path.dirname(__file__)))
+print(os.getcwd())
+
 load_dotenv()
 
 app = Flask(__name__, static_url_path='/',
@@ -11,9 +15,14 @@ app = Flask(__name__, static_url_path='/',
                   template_folder="../front-end/build")
 CORS(app)
 
+
 app.config.from_object(os.environ['APP_SETTINGS'])
+#app.config.from_object(conf.DevelopmentConfig)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 from app import routes
+
+if __name__ == "__main__":
+    app.run()
