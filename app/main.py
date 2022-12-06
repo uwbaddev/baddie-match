5999394c2841afcc6ce61d46696be5d2d5ba9a98
ivanccheng <chengivanc@gmail.com>
2022-12-05 21:33:27 -0500

    cleanup

--- app/main.py
+++ app/main.py
@@ -17,7 +17,6 @@ CORS(app)
 
 
 app.config.from_object(os.environ['APP_SETTINGS'])
-#app.config.from_object(conf.DevelopmentConfig)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
