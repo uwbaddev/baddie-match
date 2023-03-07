4e483434cc11e6f61bfe92047df63354df0b1e7e
Darren Choi <75344587+choiii-darren@users.noreply.github.com>
2023-03-07 16:06:43 -0500

    updated all matches url

--- front-end/src/API/API.js
+++ front-end/src/API/API.js
@@ -3,7 +3,7 @@ export const MatchUrl = (id) => `${DomainName}/match/${id}`
 export const ReportMatchUrl = `${DomainName}/match`
 export const CategoryUrl = `${DomainName}/categories`;
 export const PlayersUrl = `${DomainName}/players`
-export const AllMatchesUrl = `${DomainName}/matches`;
+export const AllMatchesUrl = `${DomainName}/matches/all`;
 export const PlayerMatchesUrl = (id) => `${DomainName}/match/player/${id}`
 export const PlayerIdUrl = (id) => `${DomainName}/player/${id}`
 export const CategoryId = (id) => `${DomainName}/category/${id}`
