a2a0f8824ccc0339f6c62b20ed3371c1120a4970
worker <i22cheng@uwaterloo.ca>
2023-09-10 19:03:38 -0400

    chill

---
+++ app/migrations/versions/8881c24baf1f_add_elo_active_to_players.py
@@ -0,0 +1,24 @@
+"""add elo, active to players
+
+Revision ID: 8881c24baf1f
+Revises: d4147579035a
+Create Date: 2023-09-10 19:02:27.415347
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8881c24baf1f'
+down_revision = 'd4147579035a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
